d547de4c5a088775f81eaaa459757538ed8cbc15
Udita Bose <ub298@nyu.edu>
2017-07-26 01:16:18 -0400

    Updated

--- git-commit.sh
+++ git-commit.sh
@@ -28,18 +28,6 @@
 
 source ./colors.sh
 
-#Color_Off='\033[0m'       # Text Reset
-
-# Regular Colors
-#Black='\033[0;30m'        # Black
-#Red='\033[0;31m'          # Red
-#Green='\033[0;32m'        # Green
-#Yellow='\033[0;33m'       # Yellow
-#Blue='\033[0;34m'         # Blue
-#Purple='\033[0;35m'       # Purple
-#Cyan='\033[0;36m'         # Cyan
-#White='\033[0;37m'        # White
-
 echo -e $Yellow'\n********* Updating git repos **********\n'$Color_Off
 
 GIT_NOT_MODIFIED_STATUS='nothing to commit, working directory clean'
